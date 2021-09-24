e09b5887a55a0d497eb93485a5706356ce7e9959
Disneyteam <87754364+Disneyteam77@users.noreply.github.com>
2021-09-24 17:23:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 """
-VideoPlayerBot, Telegram Video Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
+Radio-Video-Streamer, Telegram Video Chat Bot
+Copyright (c) 2021  Disney Team77 <https://github.com/disneyteam77>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
@@ -29,9 +29,9 @@ API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 SESSION_STRING = getenv("SESSION_STRING", "")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "SafoTheBot")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "AsmSafone")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "MyVideoPlayer")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "disneyteamchat")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "disneygrou")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Disneyteamvideoplayerbot")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 REPLY_MESSAGE = getenv("REPLY_MESSAGE", "")
 if REPLY_MESSAGE:
